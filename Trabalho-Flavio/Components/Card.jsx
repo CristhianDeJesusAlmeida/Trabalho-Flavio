29c39e9a0cc69616392bedd9a3cd4f32bf5df564
CristhianDeJesusAlmeida <gatinho_sky432@hotmail.com>
2026-04-12 13:38:05 -0300

    Trabalho v07: Adicionado css,, fonte no index e arrumando o card com adição de ternarios

--- Trabalho-Flavio/Components/Card.jsx
+++ Trabalho-Flavio/Components/Card.jsx
@@ -1,12 +1,17 @@
-export const Card = ({ nome, tipo, descricao, image,rarity, behavior }) => {
+export const Card = ({ nome, tipo, descricao, image, rarity, behavior }) => {
   return (
     <div className="card">
       <img src={image} alt={nome} />
       <h2>{nome}</h2>
-      {rarity && <h3><strong>Raridade:</strong> {rarity}</h3>}
-      {behavior && <h3><strong>Comportamento:</strong> {behavior}</h3>}
+      {tipo === "Item" ? (
+        <h3><strong>Raridade:</strong> {rarity}</h3>
+      ) : null}
+      {tipo !== "Item" ? (
+        <h3><strong>Comportamento:</strong> {behavior}</h3>
+      ) : null}
+
       <h3>Tipo: {tipo}</h3>
       <p>{descricao}</p>
     </div>
-  )
-}
+  );
+};
